52a0172b9eba5ba8a74a7701af25125897a507d2
AndrewVVo <anh-vu.vo@ucdconnect.ie>
2016-09-02 18:32:48 +0100

    Initialize

---
+++ map.js
@@ -0,0 +1,17 @@
+window.onload = getMyLocation;
+
+function getMyLocation(){
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(displayLocation);
+	} else {
+		alert("Oops, no geolocation support");
+	}
+}
+
+function displayLocation(){
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	alert("h" + longitude);
+	var div = document.getElementById("location");
+	div.innerHTML = "You are at Lat " + latitude + ", Long " + longitude;
+}
