7f9b2983156f22cfedc3ff5b65561a46ccc638c4
AndrewVVo <anh-vu.vo@ucdconnect.ie>
2016-09-02 18:53:54 +0100

    add google map

--- map.js
+++ map.js
@@ -14,3 +14,16 @@ function displayLocation(position){
 	var div = document.getElementById("location");
 	div.innerHTML = "You are at Lat: " + latitude + ", Long: " + longitude;
 }
+
+var map;
+
+function showMap(coords){
+	var googleLatLon = new google.maps.LatLng(coords.latitude, coords.longitude);
+	var mapOptions={
+		zoom=10,
+		center: googleLatLon,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var mapDiv = document.getElementById("map");
+	map=new google.maps.Map(mapDiv, mapOptions);
+}
