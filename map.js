5ef7aa7161b78a8c7dfc0db81880bd9e62a530bf
AndrewVVo <anh-vu.vo@ucdconnect.ie>
2016-09-03 17:45:33 +0100

    google example

--- map.js
+++ map.js
@@ -2,6 +2,7 @@ window.onload = getMyLocation;
 
 function getMyLocation(){
 	if(navigator.geolocation){
+		alert("d");
 		navigator.geolocation.getCurrentPosition(displayLocation);
 	} else {
 		alert("Oops, no geolocation support");
@@ -13,19 +14,6 @@ function displayLocation(position){
 	var longitude = position.coords.longitude;
 	var div = document.getElementById("location");
 	div.innerHTML = "You are at Lat: " + latitude + ", Long: " + longitude;
-	
-	initMap(position.coords);
 }
 
 var map;
-
-function initMap(coords){
-	var googleLatLon = new google.maps.LatLng(coords.latitude, coords.longitude);
-	var mapOptions={
-		zoom: 10,
-		center: googleLatLon,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var mapDiv = document.getElementById("map");
-	map=new google.maps.Map(mapDiv, mapOptions);
-}
